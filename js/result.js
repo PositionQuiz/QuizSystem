d8b6f2a7b16cdaceee6f2487eec5f2a5a6d30853
kholoud khraisat <kholoudmahmoud.kh@gmail.com>
2021-10-23 00:50:46 +0300

    theme functionality done

---
+++ js/result.js
@@ -0,0 +1,19 @@
+let ratio=document.getElementById("percentage1");
+
+
+if(ratio.innerHTML>=60) {
+    document.getElementById("percentage1").style.color="green";
+    document.getElementById("percentage").style.color="green";
+    document.getElementById("score").style.color="green";
+    document.getElementById("score1").style.color="green";
+   
+
+
+}else{    document.getElementById("percentage1").style.color="red";
+document.getElementById("percentage").style.color="red";
+document.getElementById("score").style.color="red";
+document.getElementById("score1").style.color="red";
+
+
+
+}
