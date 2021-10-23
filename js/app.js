2d44521d70b73afc7d026e989fb90d3ef3bdc52e
yaserananbeh <yaser.ananbeh74@gmail.com>
2021-10-23 15:56:54 +0300

    complete the registeration funcitonality

--- js/app.js
+++ js/app.js
@@ -51,24 +51,78 @@ function toggleActive(array,activeElement){
 
 const loginForm=document.querySelector("form[name='login']");
 loginForm.addEventListener("submit",trySomething)
+/*-----------------------*/
+/*-------- prevent the refresh -----*/
 function trySomething(e){
     e.preventDefault();
 }
+
+class User{
+    constructor(username,password){
+        this.username=username;
+        this.password=password;
+    }
+}
+
+let usersArray=[];
+/******************************* */
+/**********save the previous data storage ********* */
+/******************************* */
+if(localStorage){
+let storageData=JSON.parse( localStorage.getItem("users"));
+storageData.forEach((data)=>{
+    usersArray.push(data);
+})
+
+}
+
+
 const registerForm=document.querySelector("form[name='Register']");
 registerForm.addEventListener("submit",trySomething)
 function trySomething(e){
     e.preventDefault();
+    /*************************** */
+    /**********fetch the user inputs******** */
+    let username=e.target.username.value;
+    let password=e.target.password.value;
+    let password2=e.target.password2.value;
+    /************************ */
+    /********check match passwords********* */
+    if (password!==password2) {
+        alert("the passwords not match")
+    }
+    else{
+        /**************** */
+        /****check if the user exist ****** */
+        for (let i = 0; i < usersArray.length; i++) {
+            if (usersArray[i].username==username) {
+                alert("You have registered before please sign in");
+                return;
+            }
+        }
+        /*****add the user if not exist before****** */
+        let newUser=new User(username,password);
+        usersArray.push(newUser)
+        localStorage.setItem("users", JSON.stringify(usersArray) )
+        
+    }
+    
+
+
 }
 
 const regesterLink=document.querySelector("form[name='login'] td a.registerLink");
 const goToLoginLink=document.querySelector("form[name='Register'] td a.goToLoginLink");
+
+
 goToLoginLink.addEventListener("click",showLoginSection)
 console.log(regesterLink)
+
 regesterLink.addEventListener("click",showRegisterSection)
 function showRegisterSection(){
     toggleActive(columns,columns[3]);
 }
-
+/************************************************* */
 
 
 
