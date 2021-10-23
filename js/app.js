f969bd8d07abdb7989998bab1a21ba083b558691
“Azmi <AzmiTammam@gmail.com>
2021-10-23 17:53:04 +0300

    Finsh the login and Register

--- js/app.js
+++ js/app.js
@@ -1,85 +1,107 @@
 // -----------------------
 // ------- target the section buttons-------
 const testDescBtn = document.querySelector(".column.pd button");
-const positionDescLink=document.querySelector("header nav a.positionDescLink")
-const testDescLink=document.querySelector("header nav a.testDescLink")
-const loginToStartBtn=document.querySelector(".column.td button")
+const positionDescLink = document.querySelector(
+  "header nav a.positionDescLink"
+);
+const testDescLink = document.querySelector("header nav a.testDescLink");
+const loginToStartBtn = document.querySelector(".column.td button");
 
-
-const links=document.querySelectorAll("header nav ul li a");
-const columns=document.querySelectorAll(".row .column")
-
+const links = document.querySelectorAll("header nav ul li a");
+const columns = document.querySelectorAll(".row .column");
 
 
 // ----------------------------
 // ----------- the events functionality --------
-links[0].addEventListener("click",showPositionDescSection)
-function showPositionDescSection(){
-    toggleActive(columns,columns[0]);
-    toggleActive(links,links[0]);
-  };
+links[0].addEventListener("click", showPositionDescSection);
+function showPositionDescSection() {
+  toggleActive(columns, columns[0]);
+  toggleActive(links, links[0]);
+}
 
 links[1].addEventListener("click", showTestSection);
 testDescBtn.addEventListener("click", showTestSection);
-function showTestSection(){
-
-    toggleActive(columns,columns[1]);
-    toggleActive(links,links[1]);
-};
+function showTestSection() {
+  toggleActive(columns, columns[1]);
+  toggleActive(links, links[1]);
+}
 
 links[2].addEventListener("click", showContactUsSection);
-function showContactUsSection(){
-    toggleActive(columns,columns[4]);
-    toggleActive(links,links[2]);
-};
-
-loginToStartBtn.addEventListener("click",showLoginSection)
-links[3].addEventListener("click", showLoginSection);
-function showLoginSection(){
-    toggleActive(columns,columns[2]);
-    toggleActive(links,links[3]);
-};
-
-
-function toggleActive(array,activeElement){
-    array.forEach((element)=>{
-        element.classList.remove("active");
-    })
-    activeElement.classList.add("active")
+function showContactUsSection() {
+  toggleActive(columns, columns[4]);
+  toggleActive(links, links[2]);
 }
 
+loginToStartBtn.addEventListener("click", showLoginSection);
+links[3].addEventListener("click", showLoginSection);
+function showLoginSection() {
+  toggleActive(columns, columns[2]);
+  toggleActive(links, links[3]);
+}
 
-const loginForm=document.querySelector("form[name='login']");
-loginForm.addEventListener("submit",trySomething)
+function toggleActive(array, activeElement) {
+  array.forEach((element) => {
+    element.classList.remove("active");
+  });
+  activeElement.classList.add("active");
+}
+
+const loginForm = document.querySelector("form[name='login']");
+loginForm.addEventListener("submit", loginTheUser);
 /*-----------------------*/
 /*-------- prevent the refresh -----*/
-function trySomething(e){
-    e.preventDefault();
+function loginTheUser(e) {
+  e.preventDefault();
+  let username = e.target.username.value;
+  let password = e.target.password.value;
+
+
+  let exist = false;
+
+  for (let i = 0; i < usersArray.length; i++) {
+    if (usersArray[i].username==username && usersArray[i].password==password) {
+
+        toggleActive(columns, columns[5]);
+        document.getElementById("showUserName").innerHTML = username;
+        let loginLink = document.querySelector(".loginLink");
+        console.log (loginLink)
+        loginLink.innerHTML = username;
+
+        links[0].style.display = "none";
+        links[1].style.display = "none";
+        links[2].style.display = "none";
+
+        links[3].removeEventListener("click", showLoginSection);
+        exist = true;
+        return;
+    } 
+}
+if (!exist) {
+    alert("Your Username or Password is incorrect!");
+}
 }
 
-class User{
-    constructor(username,password){
-        this.username=username;
-        this.password=password;
-    }
+class User {
+  constructor(username, password) {
+    this.username = username;
+    this.password = password;
+  }
 }
 
-let usersArray=[];
+let usersArray = [];
 /******************************* */
 /**********save the previous data storage ********* */
 /******************************* */
-if(localStorage){
-let storageData=JSON.parse( localStorage.getItem("users"));
-storageData.forEach((data)=>{
+if (localStorage.getItem("users")) {
+  let storageData = JSON.parse(localStorage.getItem("users"));
+  storageData.forEach((data) => {
     usersArray.push(data);
-})
-
+  });
 }
 
-
-const registerForm=document.querySelector("form[name='Register']");
-registerForm.addEventListener("submit",trySomething)
-function trySomething(e){
+const registerForm = document.querySelector("form[name='Register']");
+registerForm.addEventListener("submit", RegisterTheUser);
+function RegisterTheUser(e){
     e.preventDefault();
     /*************************** */
     /**********fetch the user inputs******** */
@@ -94,11 +116,29 @@ function trySomething(e){
     else{
         /**************** */
         /****check if the user exist ****** */
+
+        let exist = false;
+
         for (let i = 0; i < usersArray.length; i++) {
             if (usersArray[i].username==username) {
                 alert("You have registered before please sign in");
+
+                exist = true;
                 return;
             }
+        }
+        if(!exist) {
+            toggleActive(columns, columns[5]);
+            document.getElementById("showUserName").innerHTML = username;
+            let loginLink = document.querySelector(".loginLink");
+            console.log (loginLink)
+            loginLink.innerHTML = username;
+    
+            links[0].style.display = "none";
+            links[1].style.display = "none";
+            links[2].style.display = "none";
+    
+            links[3].removeEventListener("click", showLoginSection);
         }
         /*****add the user if not exist before****** */
         let newUser=new User(username,password);
@@ -111,20 +151,18 @@ function trySomething(e){
 
 }
 
-const regesterLink=document.querySelector("form[name='login'] td a.registerLink");
-const goToLoginLink=document.querySelector("form[name='Register'] td a.goToLoginLink");
+const regesterLink = document.querySelector(
+  "form[name='login'] td a.registerLink"
+);
+const goToLoginLink = document.querySelector(
+  "form[name='Register'] td a.goToLoginLink"
+);
 
+goToLoginLink.addEventListener("click", showLoginSection);
+console.log(regesterLink);
 
-goToLoginLink.addEventListener("click",showLoginSection)
-console.log(regesterLink)
-
-regesterLink.addEventListener("click",showRegisterSection)
-function showRegisterSection(){
-    toggleActive(columns,columns[3]);
+regesterLink.addEventListener("click", showRegisterSection);
+function showRegisterSection() {
+  toggleActive(columns, columns[3]);
 }
 /************************************************* */
-
-
-
-
-
