7cc4399384fb5013bb0b33abd04010bd734dfebb
Msallam-daas <msallam.daas@gmail.com>
2021-10-24 14:48:39 +0300

    show questions&user answer

--- js/answers.js
+++ js/answers.js
@@ -1,26 +1,22 @@
+ let Quiz=JSON.parse(localStorage.getItem("Quiz"));
+ let usersAnswerArray=JSON.parse(localStorage.getItem("Answers"));
+let questionsArray=[];
+let answersArray=[];
+for (let i = 0; i < Quiz.length; i++) {
+    questionsArray.push(Quiz[i].question);
+    answersArray.push(usersAnswerArray[i]);
+//   console.log(Quiz[i].answer);    
+}
 let answersContainer=document.querySelector(".Answers");
-const questionsArray=["Question1","Question2","Question3","Question4","Question5"];
-const answersArray=["Answer1","Answer2","Answer3","Answer4","Answer5"];
 for(i=0;i<questionsArray.length;i++){
-    console.log(questionsArray[i]);
     let newDiv=document.createElement("div");
     newDiv.setAttribute("class",`Question${i+1}`);
-    console.log(newDiv);
     newDiv.innerHTML=questionsArray[i];
     answersContainer.append(newDiv);
-
     let newSection=document.createElement("section");
     newSection.setAttribute("id",`ans${i+1}`);
-    console.log(newSection);
-  
     newDiv.append(newSection);
     let newParagraph=document.createElement("p");
     newParagraph.innerHTML=answersArray[i];
     newSection.append(newParagraph);
-
-
-
-
-
-
 }
